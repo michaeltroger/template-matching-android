d4cfb41ed46742d13e5c83a88c328ef97e8cd26d
Michael Troger <dev@michaeltroger.com>
2021-08-19 22:07:04 +0200

    Undo

--- app/src/main/java/com/michaeltroger/templatematching/MainActivity.kt
+++ app/src/main/java/com/michaeltroger/templatematching/MainActivity.kt
@@ -112,7 +112,7 @@ class MainActivity : Activity(), CvCameraViewListener2 {
         super.onResume()
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization")
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback)
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback)
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it!")
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
